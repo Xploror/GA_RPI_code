5db1aafb84035ecfadcef1cc5899d34371917219
Suryansh Aryan <69386934+Xploror@users.noreply.github.com>
2022-06-29 11:48:30 +0530

    Add files via upload

---
+++ util/testutil.py
@@ -0,0 +1,347 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul  8 14:07:56 2020
+
+@author: Sachchit Vekaria
+@Organization: General Aeronautics Pvt Ltd
+"""
+
+# import necessary modules
+import time
+from unittest import FunctionTestCase
+from util.gacommonutil import CompanionComputer, mavutil, ScheduleTask
+import threading
+import logging
+
+
+# # # Another module
+import math
+import numpy as np
+#import matplotlib.pyplot as plt
+import util.VectorMath as vmath
+import util.SAADriver as driver
+import util.SAADataHandling as estimation
+import util.SAAController as control
+
+
+class TestCompanionComputer(CompanionComputer):
+    def __init__(self, sitlType, sitlPort):
+        # Initialize super class
+        super().__init__(sitlType, sitlPort)
+        
+        # Threading Lock for TestCompanionComputer Class
+        self.lock = threading.Lock()
+        self.handleRecievedMsgThread = None
+
+        #Initialise SITL driver
+        self.lidar = driver.SensorDriver('RPLidar')
+
+        #Connect to the listener - ensure the listener is running in background!!
+        self.lidar.connect_and_fetch()
+
+        #Front sensor
+        #self.front_sensor = estimation.Sensor(1,1*math.pi/180,12,0.01,0)
+        #SITL
+        self.front_sensor = estimation.Sensor(1, 1*math.pi/180, 12, 0.01, 0)  
+
+        #Initialise pre processor
+        self.coordinate_transform = estimation.DataPreProcessor()
+
+        #initialise navigation controller
+        self.navigation_controller = control.ObstacleAvoidance(max_obs=10)
+
+        self.navigation_map = estimation.DataPostProcessor()
+
+        #Initialize vector class of Vectormath
+        self.vec = vmath.vector()
+
+
+        #Brake
+        self.brake = 0
+        self.alreadybraked = 0
+        self.initvar=1
+
+        #Initiatialising Position Predictor Vectors
+        self.prev_px = 0
+        self.prev_py = 0
+        self.px = 0
+        self.py = 0
+
+        # Terminate trigger
+        self.terminate = 0
+
+        
+    def init(self):
+        super().init()
+
+        # set data stream rate
+        self.set_data_stream()
+        
+        # start our recieving message handling loop
+        self.handleRecievedMsgThread = threading.Thread(target=self.handle_recieved_message)
+        self.handleRecievedMsgThread.start()
+
+        ### Starting reading threads as they are while loops ###
+        t1 = threading.Thread(target=self.lidar.give_scan_values)
+        t1.start()
+        t2 = threading.Thread(target=self.lidar.read_fast)
+        t2.start()
+        
+        
+        # set data stream rate
+        self.set_data_stream()
+        
+        # start our recieving message handling loop
+        self.handleRecievedMsgThread = threading.Thread(target=self.handle_recieved_message)
+        self.handleRecievedMsgThread.start()
+
+        #Scheduled the threads
+        #self.scheduledTaskList.append(ScheduleTask(0.02, self.lidar.update_sitl_sensor))
+        self.scheduledTaskList.append(ScheduleTask(0.008, self.update_vars))
+        self.scheduledTaskList.append(ScheduleTask(0.5, self.previous_position_storer))
+        #self.scheduledTaskList.append(ScheduleTask(0.000000000000000001, self.lidar.give_scan_values))
+        #self.give_scan_values()
+        self.scheduledTaskList.append(ScheduleTask(0.00002,self.lidar.update_rplidar))
+        self.scheduledTaskList.append(ScheduleTask(0.02,self.front_sensor.handle_raw_data))
+        self.scheduledTaskList.append(ScheduleTask(0.02, self.coordinate_transform.update_vehicle_states))
+        self.scheduledTaskList.append(ScheduleTask(0.02, self.coordinate_transform.convert_body_to_inertial_frame))
+
+        self.scheduledTaskList.append(ScheduleTask(0.5, self.navigation_controller.predict_pos_vector)) # previously 0.5
+        self.scheduledTaskList.append(ScheduleTask(0.01, self.navigation_controller.basic_stop))
+        #self.scheduledTaskList.append(ScheduleTask(0.01, self.navigation_controller.Guided_navigation))
+        self.scheduledTaskList.append(ScheduleTask(0.01, self.handbrake))
+        self.scheduledTaskList.append(ScheduleTask(0.01, self.trigger_avoidance))
+        #self.scheduledTaskList.append(ScheduleTask(0.05, self.check_mode(False)))
+        self.scheduledTaskList.append(ScheduleTask(0.01, self.maneuver))
+        self.scheduledTaskList.append(ScheduleTask(0.05,self.navigation_stack))
+        self.scheduledTaskList.append(ScheduleTask(0.02,self.navigation_map.forget_far_obstacles))  # default was 1
+        self.scheduledTaskList.append(ScheduleTask(0.02,self.termination))
+        # self.scheduledTaskList.append(ScheduleTask(0.05,self.debug))
+
+
+
+        while True:
+            if self.navigation_controller.obstacle_map is None:
+                pass
+            else:
+                time.sleep(0.01)
+
+    def handbrake(self):
+        if self.initvar==1:
+            print("Handbrake Initialize")
+            self.initvar = 0
+        #Only brake when previously brake command is not given, altitude is greater than 1
+        if self.brake and not self.alreadybraked and self.relativeAlt > 1:
+            self.add_new_message_to_sending_queue(mavutil.mavlink.MAVLink_set_mode_message(self.mavlinkInterface.mavConnection.target_system,
+                                                                                           mavutil.mavlink.MAV_MODE_FLAG_CUSTOM_MODE_ENABLED,
+                                                                                           17))
+            self.alreadybraked = 1
+        self.check_mode(False)     # All required trigger switching in check_mode() is instantaneously done thru here (default - False)
+
+    def trigger_avoidance(self):
+        '''Triggers Guided Mode only when drone is already in Brake mode due to Basic_stop() and Guiding is not started'''
+        ######################## Equating SAAController's heading with updated heading by SAAHandling #####################
+        try:
+            heading = np.asarray(self.coordinate_transform.heading)
+            #print(heading)
+            self.navigation_controller.heading = np.array([heading[0][0], heading[0][1]])
+        except:
+            pass
+        ###################################################################################################################
+
+        ################################# Change to GUIDED mode only if it first braked  #############################################
+        if self.currentMode == 'BRAKE':
+            #print(self.alreadybraked and not self.navigation_controller.guide)
+            while not self.navigation_controller.guide:
+                self.add_new_message_to_sending_queue(mavutil.mavlink.MAVLink_set_mode_message(self.mavlinkInterface.mavConnection.target_system, 
+                                                                                            mavutil.mavlink.MAV_MODE_FLAG_CUSTOM_MODE_ENABLED,
+                                                                                            4))
+            ##print('Switched to Guided Mode for Avoidance')
+            self.check_mode(True)
+            self.navigation_controller.guiding = 1       # This logic is seperately included because guiding should be true only when in guided mode after it was braked
+        ########################################################################################################################
+                    
+        ############################ Method to switch back to AUTO mode after avoidance is done #####################################
+        if self.navigation_controller.auto:
+            self.add_new_message_to_sending_queue(mavutil.mavlink.MAVLink_set_mode_message(self.mavlinkInterface.mavConnection.target_system, 
+                                                                                           mavutil.mavlink.MAV_MODE_FLAG_CUSTOM_MODE_ENABLED,
+                                                                                           3))
+            if self.currentMode == 'AUTO':  # Why there was != and still it worked
+                self.navigation_controller.auto = 0
+            
+            print('Entered AUTO mode')
+        ###########################################################################################################################
+
+    def check_mode(self, out):
+        
+        mode = self.currentMode
+        if out:
+            print('Mode : ', mode)
+
+        if mode == 'GUIDED':
+            self.navigation_controller.guide = 1
+              
+        else:
+            self.navigation_controller.guide = 0
+            self.navigation_controller.avoided = 0
+            self.navigation_controller.avoiding = 0
+
+        if mode == 'AUTO':
+            self.navigation_controller.ctrl = 0
+            self.alreadybraked = 0     # To revert back the default value so that again the process if necessary can be repeated
+            
+        if mode == 'LOITER':
+            self.terminate = 1
+
+    def maneuver(self): 
+        #print(f'{self.navigation_controller.ctrl and self.navigation_controller.guide} --- {self.currentMode}')
+        if self.navigation_controller.ctrl and self.navigation_controller.guide:
+            # Navigate rightward unless drone sees safe angle
+            if not self.navigation_controller.stop and not self.navigation_controller.avoided:
+                self.mavlinkInterface.mavConnection.mav.send(mavutil.mavlink.MAVLink_set_position_target_local_ned_message( 10, self.mavlinkInterface.mavConnection.target_system, self.mavlinkInterface.mavConnection.target_component, mavutil.mavlink.MAV_FRAME_BODY_OFFSET_NED, int(0b010111000111), 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0))
+                print('Moving right !!!')
+
+            # Stop command in Guided mode
+            if self.navigation_controller.stop and not self.navigation_controller.avoided:
+                self.mavlinkInterface.mavConnection.mav.send(mavutil.mavlink.MAVLink_set_position_target_local_ned_message( 10, self.mavlinkInterface.mavConnection.target_system, self.mavlinkInterface.mavConnection.target_component, mavutil.mavlink.MAV_FRAME_BODY_OFFSET_NED, int(0b010111000111), 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0))
+                print('Forced Stop !')
+
+            # Moving Forward command in Guided mode
+            #print(f'                                                  {not self.navigation_controller.stop and self.navigation_controller.avoided}')
+            if not self.navigation_controller.stop and self.navigation_controller.avoided:
+                #print('                                                                                      Forward movement maneuver condition')
+                self.mavlinkInterface.mavConnection.mav.send(mavutil.mavlink.MAVLink_set_position_target_local_ned_message( 10, self.mavlinkInterface.mavConnection.target_system, self.mavlinkInterface.mavConnection.target_component, mavutil.mavlink.MAV_FRAME_BODY_OFFSET_NED, int(0b110111000111), 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0))
+                print('Moving forward !!!')
+
+    def termination(self):
+        '''Flight test safety purpose'''
+        if self.terminate == 1:
+            for task in self.scheduledTaskList:
+                task.stop()
+            print('Terminated')
+        
+
+    def previous_position_storer(self):
+        """Warning: This function should be called at a co-prime pair frequency with update_vars
+        The function relies on the fact that the current value has already been updated and in the update_vars and the 
+        previous position is stored for 1 second. 
+        [Tested in SITL]
+        """
+        self.prev_px = self.px
+        self.prev_py = self.py
+
+    def debug(self):
+        """Debugger if you want to print something
+        """
+        #print(f"New:  {self.navigation_controller.obstacle_map}")
+        # for i in range(self.navigation_controller.obstacle_map):
+        #     obstacle = [self.navigation_controller.obstacle_map[i,0], self.navigation_controller.obstacle_map[i,1]]
+        #plt.clf()
+        plt.plot(self.navigation_map.map[:,0], self.navigation_controller.obstacle_map[:,1], 'o')
+        plt.show()
+
+        
+
+
+    def update_vars(self):
+        """This function acts as a bridge between different class to transfer data. Part of the requirements for 
+        developing the algorithm
+        """
+        #6.198883056640625e-06 seconds
+        self.front_sensor.data = self.lidar.raw_data
+
+        #Gazebo axes are different from my 
+        # axis, thus I have to handle it. 
+        if self.lidar.drivername == 'SITL':
+            self.coordinate_transform.roll = self.roll + math.pi
+        else:
+            self.coordinate_transform.roll = self.roll
+        self.coordinate_transform.pitch = self.pitch
+        #Yaw wrapper to keep it compatible with my transformations
+        self.coordinate_transform.yaw = math.atan2(math.sin(self.yaw),math.cos(self.yaw))
+        self.coordinate_transform.px = self.px
+        self.coordinate_transform.py = self.py
+        self.coordinate_transform.pz = self.relativeAlt
+        self.navigation_controller.px = self.px
+        self.navigation_controller.py = self.py
+        self.navigation_controller.vx = self.vx
+        self.navigation_controller.vy = self.vy
+        self.brake = self.navigation_controller.brake
+        self.coordinate_transform.x = self.front_sensor.X
+        self.coordinate_transform.y = self.front_sensor.Y
+        self.navigation_controller.mode = self.currentMode
+        self.navigation_map.px = self.px
+        self.navigation_map.py = self.py
+
+        self.navigation_controller.prev_px = self.prev_px
+        self.navigation_controller.prev_py = self.prev_py
+
+
+
+    def navigation_stack(self):
+        """Update vars function was getting filled....
+        I am making another thread. 
+        """
+        #print(self.coordinate_transform.obstacle_vector_inertial)
+        #Angular inertial obstacle vector to grid based reading and that is stored in global map
+        self.navigation_map.convert_rel_obstacle_to_inertial(self.navigation_map.grid(self.coordinate_transform.obstacle_vector_inertial.T))
+
+        #Relative obstacle is sent to the navigation algorithm
+        self.navigation_controller.obstacle_map = self.navigation_map.convert_inertial_to_rel()
+        
+        # Storing previosly detected obstacles inertial position in self.obstacle_inertial
+        if self.navigation_controller.obstacle_inertial.size == 0:
+            self.navigation_controller.obstacle_inertial = np.array([[self.coordinate_transform.px,self.coordinate_transform.py]])
+            #self.navigation_controller.obstacle_inertial = np.array([[0,0]])
+
+        self.navigation_controller.obstacle_inertial = np.unique(np.concatenate((self.navigation_controller.obstacle_inertial,self.navigation_map.convert_rel_to_inertial_avoid(self.navigation_controller.obstacle_map)),axis=0),axis=0)
+        self.navigation_controller.obstacle_inertial = np.array([i for i in self.navigation_controller.obstacle_inertial if self.vec.mag2d(self.navigation_map.convert_inertial_to_rel_avoid(i))<30])
+        self.navigation_controller.mag_obs_inertial = np.array([self.vec.mag2d(self.navigation_map.convert_inertial_to_rel_avoid(i)) for i in self.navigation_controller.obstacle_inertial])
+
+        self.navigation_controller.obstacle_body = np.array([self.navigation_map.convert_inertial_to_rel_avoid(i) for i in self.navigation_controller.obstacle_inertial])
+
+
+         
+    def set_data_stream(self):
+        # data rate of more than 100 Hz is not possible as recieving loop is set to run at interval of 0.01 sec
+        # don't change that as it affects other vehicles as well
+    
+        # request data to be sent at the given rate
+        # arguments are (target_system, target_component, stream_type, frequency in Hz, stop(0) or start(1))
+    
+        # stop data which are coming automatically to stop recieving unnecessary messeges
+        self.add_new_message_to_sending_queue(mavutil.mavlink.MAVLink_request_data_stream_message(self.mavlinkInterface.mavConnection.target_system, self.mavlinkInterface.mavConnection.target_component, mavutil.mavlink.MAV_DATA_STREAM_ALL, 4, 0))
+
+
+        #self.add_new_message_to_sending_queue(mavutil.mavlink.MAVLink_request_data_stream_message(self.mavlinkInterface.mavConnection.target_system, self.mavlinkInterface.mavConnection.target_component, mavutil.mavlink.MAV_DATA_STREAM_ALL, 1, 1))
+        #self.add_new_message_to_sending_queue(mavutil.mavlink.MAVLink_request_data_stream_message(self.mavlinkInterface.mavConnection.target_system, self.mavlinkInterface.mavConnection.target_component, mavutil.mavlink.MAV_DATA_STREAM_RAW_SENSORS, 1, 1))
+        self.add_new_message_to_sending_queue(mavutil.mavlink.MAVLink_request_data_stream_message(self.mavlinkInterface.mavConnection.target_system, self.mavlinkInterface.mavConnection.target_component, mavutil.mavlink.MAV_DATA_STREAM_EXTENDED_STATUS, 2, 1))
+        self.add_new_message_to_sending_queue(mavutil.mavlink.MAVLink_request_data_stream_message(self.mavlinkInterface.mavConnection.target_system, self.mavlinkInterface.mavConnection.target_component, mavutil.mavlink.MAV_DATA_STREAM_RC_CHANNELS, 2, 1))
+        #self.add_new_message_to_sending_queue(mavutil.mavlink.MAVLink_request_data_stream_message(self.mavlinkInterface.mavConnection.target_system, self.mavlinkInterface.mavConnection.target_component, mavutil.mavlink.MAV_DATA_STREAM_RAW_CONTROLLER, 1, 1))
+        self.add_new_message_to_sending_queue(mavutil.mavlink.MAVLink_request_data_stream_message(self.mavlinkInterface.mavConnection.target_system, self.mavlinkInterface.mavConnection.target_component, mavutil.mavlink.MAV_DATA_STREAM_POSITION, 5, 1))
+        self.add_new_message_to_sending_queue(mavutil.mavlink.MAVLink_request_data_stream_message(self.mavlinkInterface.mavConnection.target_system, self.mavlinkInterface.mavConnection.target_component, mavutil.mavlink.MAV_DATA_STREAM_EXTRA1, 1, 1))
+        #self.add_new_message_to_sending_queue(mavutil.mavlink.MAVLink_request_data_stream_message(self.mavlinkInterface.mavConnection.target_system, self.mavlinkInterface.mavConnection.target_component, mavutil.mavlink.MAV_DATA_STREAM_EXTRA2, 1, 1))
+        self.add_new_message_to_sending_queue(mavutil.mavlink.MAVLink_request_data_stream_message(self.mavlinkInterface.mavConnection.target_system, self.mavlinkInterface.mavConnection.target_component, mavutil.mavlink.MAV_DATA_STREAM_EXTRA3, 2, 1))
+    
+        logging.info("Stream Rate have been set")
+    
+    def handle_recieved_message(self):
+        while True:
+            if self.killAllThread.is_set():
+                break
+            recievedMsg = self.get_new_message_from_recieving_queue()
+            if recievedMsg is not None:
+                super().handle_recieved_message(recievedMsg)
+            else:
+                time.sleep(0.01)
+            
+    def kill_all_threads(self):
+        logging.info("TestCompanionComputer killing all threads")
+        super().kill_all_threads()
+#        self.killAllThread.set()
+#        
+#        for task in self.scheduledTaskList:
+#            task.stop()
+#        
+        self.handleRecievedMsgThread.join()
+        logging.info("TestCompanionComputer joined all threads")
